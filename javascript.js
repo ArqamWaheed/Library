9ac13731b95e5090d748bb5efee5a085a36e0b2e
Arqam Waheed <arqam2006waheed@gmail.com>
2025-06-28 16:23:56 +0500

    Test commit on branch

--- javascript.js
+++ javascript.js
@@ -20,47 +20,6 @@ function addBookToLibrary() {
 }
 
 
-for (book of myLibrary) {
-    cardCreate(book.name, book.author, book.pages, book.readstatus);
-}
-
-function cardCreate(bookName, authorName, numPages, boolRead) {
-    let card = document.createElement("div");
-    let titleContent = document.createElement("h1");
-    let authorContent  = document.createElement("p");
-    let pagesContent  = document.createElement("p");
-    let readstatusContent = document.createElement("p");
-    let deletebutton = document.createElement("button");
-    card.classList.toggle("card");
-    titleContent.textContent = bookName;
-    authorContent.textContent = `Written by ${authorName}`;
-    pagesContent.textContent = `${numPages} pages in the book`;
-    readstatusContent.textContent = (boolRead === true) ? "Yes have read the book!" : "No have not read the book!";
-    container.appendChild(card);
-    card.appendChild(titleContent);
-    card.appendChild(authorContent);
-    card.appendChild(pagesContent);
-    card.appendChild(readstatusContent);
-    deletebutton.textContent = "❌";
-    card.appendChild(deletebutton);
-}
-
-submit.addEventListener("submit", function(event) {
-    event.preventDefault();
-    cardCreate(title.value, author.value, pages.value, readstatus.checked);
-    addBookToLibrary();
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    readstatus.checked = false;
-})
-
-container.addEventListener('click', function(e) {
-    if (e.target.tagName === 'BUTTON') {
-        let card = e.target.closest('.card');
-        container.removeChild(card);
-    }
-});
 
 
 
