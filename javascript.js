a77ba329d680cd2567396be190ed8c14437a8c09
Arqam Waheed <arqam2006waheed@gmail.com>
2025-06-30 22:07:50 +0500

    Completely IIFE'd it heh.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,3 @@
-
-// function addBookToLibrary() {
-//     let book = new Book(title.value, author.value, pages.value, readstatus.checked);
-//     myLibrary.push(book);
-// }
-
-// const Book = (function() {
-
-// })()
-
-
-
 const changeLibrary = (function() {
     _myLibrary = [];
 
@@ -22,44 +10,83 @@ const changeLibrary = (function() {
     $cardContainer = document.querySelector(".main");
     $form = document.querySelector(".form");
 
-    // Bind event Listeners
+    // Bind eventListeners
     $form.addEventListener('submit', (event) => {
         event.preventDefault();
         addBook();
+        _clearInputs();
     });
 
-    // Define events
-    function _render() {
-        $card = document.createElement("div");
-        $card.classList.toggle("card");
-        tempArr = [$bookName.value, $authorName.value, pages.value]; 
-        for (let i = 0; i < tempArr.length; i++) {
-            $p = document.createElement("p");
-            $p.textContent = tempArr[i];
-            $card.appendChild($p);
+    $cardContainer.addEventListener('click', function(event) {
+        if (event.target.tagName == "BUTTON") {
+            let id = (event.target.closest("div")).id;
+            console.log(id);
+            removeBook(id);
         }
-        $cardContainer.appendChild($card);
-    }
+    })  
 
-    function addBook(name, author, pages, readStatus) {
-        name = (typeof name === "string") ? name : $bookName.value;
-        author = (typeof author === "string") ? author : $authorName.value;
-        pages = (typeof pages === "number") ? pages : $pages.value;
-        readStatus = (typeof readStatus === "boolean") ? readStatus : $readStatus.value;
-        book = new _Book(name, author, pages, readStatus);
-        _myLibrary.push(book);
-        _render();
-    }
-
-    function removeBook() {
-
-    }
+    // Define events
 
     function _Book(name, author, pages, readstatus) {
         this.name = name;
         this.author = author;
         this.pages = pages;
         this.readstatus = readstatus;
+        this.id = crypto.randomUUID();
+    }
+
+    _Book.prototype.readInfo = function() {
+        return this.readstatus ? "Read ✅" : "Read ❌"; 
+    }
+
+    function _render() {
+        while ($cardContainer.firstChild) { // deleting all elements
+            $cardContainer.removeChild($cardContainer.firstChild);
+        }
+
+        for (let i = 0; i < _myLibrary.length; i++) { // recreating all elements
+            $card = document.createElement("div");
+            $card.classList.toggle("card");
+            tempArr = [_myLibrary[i].name, _myLibrary[i].author, _myLibrary[i].pages, _myLibrary[i].readInfo()]; 
+            for (let j = 0; j < tempArr.length; j++) {
+                $p = document.createElement("p");
+                $p.textContent = tempArr[j];
+                $card.appendChild($p);
+            }
+            $cardButton = document.createElement("button");
+            $cardButton.textContent = "❌";
+            $card.appendChild($cardButton);
+            $card.id = _myLibrary[i].id;
+            tempArr = null;
+            $cardContainer.appendChild($card);
+        }
+    }
+
+    function _clearInputs() {
+        $bookName.value = "";
+        $authorName.value = "";
+        $pages.value = "";
+        $readStatus.checked = false;
+    }
+
+    function addBook(name, author, pages, readStatus) {
+        name = (typeof name === "string") ? name : $bookName.value;
+        author = (typeof author === "string") ? author : $authorName.value;
+        pages = (typeof pages === "number") ? pages : $pages.value;
+        readStatus = (typeof readStatus === "boolean") ? readStatus : $readStatus.checked;
+        book = new _Book(name, author, pages, readStatus);
+        _myLibrary.push(book);
+        _render();
+    }
+
+
+    function removeBook(id) {
+        for (let i = 0; i < _myLibrary.length; i++) {
+            if (_myLibrary[i].id == id) {
+                _myLibrary.splice(i, 1);
+                _render();
+            };
+        }
     }
 
     // Return IIFE 
