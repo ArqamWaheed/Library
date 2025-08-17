703750d204923eca7135b72871c52735eb22bdbf
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2025-08-17 14:22:15 +0500

    Added form validation via JS only

--- javascript.js
+++ javascript.js
@@ -45,15 +45,6 @@ function cardCreate(bookName, authorName, numPages, boolRead) {
     card.appendChild(deletebutton);
 }
 
-submit.addEventListener("submit", function(event) {
-    event.preventDefault();
-    cardCreate(title.value, author.value, pages.value, readstatus.checked);
-    addBookToLibrary();
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    readstatus.checked = false;
-})
 
 container.addEventListener('click', function(e) {
     if (e.target.tagName === 'BUTTON') {
@@ -62,5 +53,70 @@ container.addEventListener('click', function(e) {
     }
 });
 
+const $titleField = document.querySelector("#title");
+const $authorField = document.querySelector("#author");
+const $pagesField = document.querySelector("#pages");
+const $errorMessage = document.querySelector(".errorMsg");
+
+submit.addEventListener("submit", function(event) {
+    event.preventDefault();
+    if ($titleField.checkValidity() && $authorField.checkValidity() && $pagesField.checkValidity()) {
+        cardCreate(title.value, author.value, pages.value, readstatus.checked);
+        addBookToLibrary();
+        title.value = "";
+        author.value = "";
+        pages.value = "";
+        readstatus.checked = false;
+        if ($errorMessage.style.display !== "none") $errorMessage.style.display = "none"; 
+    } else if ($titleField.checkValidity() === false) {
+        showErrorTitle();
+    } else if ($authorField.checkValidity() === false) {
+        showErrorAuthor();
+    } else if ($pagesField.checkValidity() === false) {
+        showErrorPages();
+    }
+})
+
+$titleField.addEventListener("input", function(event) {
+    if (!$titleField.validity.valid){
+        showErrorTitle();
+    } else $errorMessage.style.display = "none";
+})
+$authorField.addEventListener("input", function(event) {
+    if (!$authorField.validity.valid){
+        showErrorAuthor();
+    } else $errorMessage.style.display = "none";})
+$pagesField.addEventListener("input", function(event) {
+    if (!$pagesField.validity.valid){
+        showErrorPages();
+    } else $errorMessage.style.display = "none";})
+function showErrorTitle() {
+    if ($titleField.validity.valueMissing) {
+        $errorMessage.style.display = "block";
+        $errorMessage.textContent = "Enter a title!";
+    } else if ($titleField.validity.tooShort) {
+        $errorMessage.style.display = "block";
+        $errorMessage.textContent = `The title is under ${$titleField.getAttribute("minlength")} characters!`
+    } 
+}
+
+function showErrorAuthor() {
+    if ($authorField.validity.valueMissing) {
+        $errorMessage.style.display = "block";
+        $errorMessage.textContent = "Enter an author name!";
+    } else if ($authorField.validity.tooShort) {
+        $errorMessage.style.display = "block";
+        $errorMessage.textContent = `The author name is under ${$authorField.getAttribute("minlength")} characters!`;
+    }
+}
 
 
+function showErrorPages() {
+    if ($pagesField.validity.valueMissing) {
+        $errorMessage.style.display = "block";
+        $errorMessage.textContent = "Enter a the number of pages read";
+    } else if ($pagesField.validity.rangeOverflow || $pagesField.validity.rangeUnderflow) {
+        $errorMessage.style.display = "block";
+        $errorMessage.textContent = `The number of pages are out of the set boundaries`
+    }
+}
